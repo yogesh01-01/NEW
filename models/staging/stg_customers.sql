1c1c125ea670181468b771467bef8536052fa3e4
yoges morey <hekiki5395@atinjo.com>
2026-02-01 12:47:56 +0000

    yes

---
+++ models/staging/stg_customers.sql
@@ -0,0 +1,20 @@
+{{
+    config(
+        materialized='incremental',
+        incremental_strategy='merge',
+        unique_key='customer_id'
+    )
+}}
+
+select 
+  customer_id,
+  initcap(nullif(trim(first_name), '')) as first_name,
+  initcap(nullif(trim(last_name), ''))  as last_name,
+  lower(nullif(trim(email), ''))         as email,
+  cast(created_at as date)               as customer_created_at
+from {{ source('aarvi', 'customers') }}
+
+qualify row_number() over (
+  partition by customer_id 
+  order by created_at desc
+) = 1
