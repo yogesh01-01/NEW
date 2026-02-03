659ad1b6416663b21bcbe6d61978cbb3cadeda00
yoges morey <hekiki5395@atinjo.com>
2026-02-03 07:11:15 +0000

    new

---
+++ snapshots/customers_snapshot.sql
@@ -0,0 +1,15 @@
+{% snapshot customers_snapshot %}
+
+{{
+    config(
+      target_schema='snapshots',
+      unique_key='customer_id',
+      strategy='timestamp',
+      updated_at='created_at'
+    )
+}}
+
+select *
+from {{ source('aarvi', 'customers') }}
+
+{% endsnapshot %}
