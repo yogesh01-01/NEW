9a02fcc51a222058bd2b08530a49dd0f10f744cf
yoges morey <hekiki5395@atinjo.com>
2026-02-08 06:46:52 +0000

    new

--- snapshots/payments_snapshot.sql
+++ snapshots/payments_snapshot.sql
@@ -15,12 +15,16 @@ with source_data as (
     select
         PAYMENT_ID,
         ORDER_ID,
+
+        -- clean payment method
         upper(trim(PAYMENT_METHOD)) as PAYMENT_METHOD,
+
+        -- normalize status
         lower(trim(PAYMENT_STATUS)) as raw_status,
+
         UPDATED_AT
     from {{ source('yogesh', 'payments') }}
-    where payment_id is not null
-
+    where PAYMENT_ID is not null
 )
 
 select
